11595438d5ce2393d8102dfe08e1517b3720f6b2
danilojmarins <danilojmarins@hotmail.com>
2022-03-18 14:23:57 -0300

    mialchimp post implemented

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
+const https = require('https');
+const { response } = require('express');
 const app = express();
 const port = 3000;
 
@@ -22,12 +25,60 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
     
-    let firstName = req.body.firstname.trim().charAt(0).toUpperCase() + req.body.firstname.trim().slice(1).toLowerCase();
-    let lastName = req.body.lastname.trim().charAt(0).toUpperCase() + req.body.lastname.trim().slice(1).toLowerCase();
-    let email = req.body.email.trim().toLowerCase();
+    const firstName = req.body.firstname.trim().charAt(0).toUpperCase() + req.body.firstname.trim().slice(1).toLowerCase();
+    const lastName = req.body.lastname.trim().charAt(0).toUpperCase() + req.body.lastname.trim().slice(1).toLowerCase();
+    const email = req.body.email.trim().toLowerCase();
     console.log(firstName + ' ' + lastName);
     console.log(email);
 
-    res.sendFile(__dirname + '/success.html');
+
+    const data = {
+        members: [{
+            email_address: email,
+            status: 'subscribed',
+            merge_fields: {
+                FNAME: firstName,
+                LNAME: lastName
+            }
+        }]
+    }
+
+    const jsonData = JSON.stringify(data);
+
+    const url = 'https://us14.api.mailchimp.com/3.0/lists/afdb0fc161';
+
+    const options = {
+        method: 'POST',
+        auth: 'danilojmarins:ea7286511242ced359ff00d3f9689139-us14'
+    }
+
+    const request = https.request(url, options, (response) => {
+
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + '/success.html');
+        } else {
+            res.sendFile(__dirname + '/failure.html');
+        }
+
+        response.on('data', (data) =>{
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
 
 })
+
+
+app.post('/failure', (req, res) => {
+    res.redirect('/');
+})
+
+
+
+// API Key
+// ea7286511242ced359ff00d3f9689139-us14
+
+// List ID
+// afdb0fc161
